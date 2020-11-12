31169580c279648620af2c7172aec7b028956617
shalvaX <shalva.turashvili.1@btu.edu.ge>
2020-11-12 21:11:06 +0400

    one last

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', '\App\Http\Controllers\CarController@viewCars')
     ->name('cars.all')
     ->middleware('guest');
 Route::post('/cars/create', '\App\Http\Controllers\CarController@createCar')->name('cars.save');
-Route::get('/cars/{id}/edit', '\App\Http\Controllers\ProductController@editCar')->name('carss.edit');
-Route::post('/cars/{id}/update', '\App\Http\Controllers\ProductController@updateCar')->name('cars.update');
-Route::post('/cars/delete', '\App\Http\Controllers\ProductController@deleteCar')->name('cars.delete');
+Route::get('/cars/{id}/edit', '\App\Http\Controllers\CarController@editCar')->name('carss.edit');
+Route::post('/cars/{id}/update', '\App\Http\Controllers\CarController@updateCar')->name('cars.update');
+Route::post('/cars/delete', '\App\Http\Controllers\CarController@deleteCar')->name('cars.delete');
